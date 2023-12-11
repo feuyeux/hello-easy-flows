29be357d94ed1bba452417fd6e379ae2b9e8a94f
feuyeux <feuyeux@gmail.com>
2023-12-11 17:05:38 +0800

    up

--- src/main/java/org/feuyeux/workflow/HelloEasyFlows.java
+++ src/main/java/org/feuyeux/workflow/HelloEasyFlows.java
@@ -31,36 +31,42 @@ public class HelloEasyFlows {
         RepeatFlow repeatFlow2 = buildRepeatFlow(work1, 3);
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         ParallelFlow parallelFlow = buildParallelFlow(work1, work2, work3, executorService);
-
+        log.info("=== EXECUTE CONDITIONAL FLOW ===");
         for (int i = 0; i < 3; i++) {
-            log.info("EXECUTE CONDITIONAL FLOW[{}]:", i);
+            log.info("{}", i + 1);
             WorkReport workReport = conditionalFlow.execute(workContext);
             log.info("latest flow status:{}", workReport.getStatus());
         }
-        log.info("====");
+        log.info("");
 
-        log.info("EXECUTE SEQUENTIAL FLOW:");
-        WorkReport workReport = sequentialFlow.execute(workContext);
+        log.info("=== EXECUTE SEQUENTIAL FLOW ===");
+        for (int i = 0; i < 3; i++) {
+            log.info("{}", i + 1);
+            WorkReport workReport = sequentialFlow.execute(workContext);
+            log.info("latest flow status:{}", workReport.getStatus());
+        }
+        log.info("");
+
+        log.info("=== REPEAT UNTIL FLOW ===");
+        WorkReport workReport = repeatFlow1.execute(workContext);
         log.info("latest flow status:{}", workReport.getStatus());
-        log.info("====");
+        log.info("");
 
-        log.info("REPEAT UNTIL FLOW:");
-        workReport = repeatFlow1.execute(workContext);
-        log.info("latest flow status:{}", workReport.getStatus());
-        log.info("====");
-
-        log.info("REPEAT TIMES FLOW:");
+        log.info("=== REPEAT TIMES FLOW ===");
         workReport = repeatFlow2.execute(workContext);
         log.info("latest flow status:{}", workReport.getStatus());
-        log.info("====");
+        log.info("");
 
-        log.info("PARALLEL FLOW:");
-        ParallelFlowReport parallelFlowReport = parallelFlow.execute(workContext);
-        List<WorkReport> reports = parallelFlowReport.getReports();
-        for (WorkReport report : reports) {
-            log.info(" status:{}", report.getStatus());
+        log.info("PARALLEL FLOW ===");
+        for (int i = 0; i < 3; i++) {
+            log.info("{}", i + 1);
+            ParallelFlowReport parallelFlowReport = parallelFlow.execute(workContext);
+            List<WorkReport> reports = parallelFlowReport.getReports();
+            for (WorkReport report : reports) {
+                log.info(" status:{}", report.getStatus());
+            }
+            log.info("parallel flow status:{}", parallelFlowReport.getStatus());
         }
-        log.info("parallel flow status:{}", parallelFlowReport.getStatus());
         executorService.shutdown();
     }
 
